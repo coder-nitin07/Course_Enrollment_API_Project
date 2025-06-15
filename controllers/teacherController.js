dcbd14fd60de8aedb6802bfcc0957666c5e9811a
coder-nitin07 <nitinthakur87504@gmail.com>
2025-06-15 22:42:07 +0530

    Changes in teacherController file

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -3,3 +3,15 @@
 // POST /teacher/course (create a course)
 // PUT /teacher/course/:id (update course)
 // DELETE /teacher/course/:id (delete course)
+
+// Create Teacher
+const onboardTeacher = async (req, res)=>{
+    try {
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
+module.exports = { onboardTeacher };
